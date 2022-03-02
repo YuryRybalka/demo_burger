6a3b177fdd1a3c252b85f8b4d8be60ce8db58aea
YuryRybalka <4flutter.study@gmail.com>
2022-03-02 22:04:07 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class NavigationDrawerScreen extends StatefulWidget {
+  const NavigationDrawerScreen({Key? key}) : super(key: key);
+
+  @override
+  _NavigationDrawerScreenState createState() => _NavigationDrawerScreenState();
+} // class NavigationDrawerScreen
+
+class _NavigationDrawerScreenState extends State<NavigationDrawerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.blue
+                ),
+                child: Container(
+                  height: 200,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        height: 100,
+                        decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(50.0))
+                        ),
+                        child: Image.network('https://www.chery.ru/360/exterior/tiggo4/images/blue/13.jpg'),
+                      ),
+                      const Text('Навигация Flutter'),
+                    ],
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: const Icon(Icons.one_k),
+                title: const Text('Каталог'),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: const Icon(Icons.two_k),
+                title: const Text('Корзина'),
+                onTap: () {},
+              ),
+              const Divider(),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('Профиль'),
+              ),
+              ListTile(
+                leading: const Icon(Icons.settings),
+                title: const Text('Настройки'),
+                onTap: () {},
+              ),
+            ],
+          )
+        ),
+        body: Center(child: Text('Содержимое экрана'))
+      ),
+    );
+  } // build
+} // class _NavigationDrawerScreenState
+
+void main() {
+  runApp(const NavigationDrawerScreen());
+}
